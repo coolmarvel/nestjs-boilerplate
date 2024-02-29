27d1b37294ffe6790328fa4c6b430ada9ae4e04a
coolmarvel <marvel97@naver.com>
2024-02-29 16:20:02 +0900

    request module for http protocol generate & apply to common module

---
+++ src/common/request/middleware/timezone/request.timezone.middleware.ts
@@ -0,0 +1,12 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Response, NextFunction } from 'express';
+import { IRequestApp } from 'src/common/request/interfaces/request.interface';
+
+@Injectable()
+export class RequestTimezoneMiddleware implements NestMiddleware {
+  async use(req: IRequestApp, res: Response, next: NextFunction): Promise<void> {
+    req.__timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    
+    next();
+  }
+}
