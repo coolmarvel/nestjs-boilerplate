9b14501a8d0c9e7a13e46e45cb568f459791e656
coolmarvel <marvel97@naver.com>
2024-02-29 16:58:49 +0900

    api-key module generate and apply to common

---
+++ src/common/api-key/serializations/api-key.list.serialization.ts
@@ -0,0 +1,4 @@
+import { OmitType } from '@nestjs/swagger';
+import { ApiKeyGetSerialization } from 'src/common/api-key/serializations/api-key.get.serialization';
+
+export class ApiKeyListSerialization extends OmitType(ApiKeyGetSerialization, ['description'] as const) {}
