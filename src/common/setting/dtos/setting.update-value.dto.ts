8d843d2fc8c288e35f3015d401b51a51107723d6
coolmarvel <marvel97@naver.com>
2024-02-29 13:36:26 +0900

    setting module for application generate & apply to common

---
+++ src/common/setting/dtos/setting.update-value.dto.ts
@@ -0,0 +1,4 @@
+import { OmitType } from '@nestjs/swagger';
+import { SettingCreateDto } from './setting.create.dto';
+
+export class SettingUpdateValueDto extends OmitType(SettingCreateDto, ['name', 'description'] as const) {}
