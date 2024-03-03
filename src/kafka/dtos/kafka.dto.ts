0efaea3b20df57b971451289af8163c2aed3b0d6
coolmarvel <marvel97@naver.com>
2024-03-03 09:59:36 +0900

    kafka module for connect and pub/sub service apply to app

---
+++ src/kafka/dtos/kafka.dto.ts
@@ -0,0 +1,27 @@
+import { Type } from 'class-transformer';
+import { IsString, IsNotEmpty, IsMongoId, IsBoolean, IsNumber, IsDate } from 'class-validator';
+
+export class KafkaDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly test: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @Type(() => Number)
+  readonly testNumber: number;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  readonly testBoolean: boolean;
+
+  @IsDate()
+  @IsNotEmpty()
+  @Type(() => Date)
+  readonly testDate: Date;
+
+  @IsMongoId()
+  @IsNotEmpty()
+  @Type(() => String)
+  readonly testObjectId: string;
+}
